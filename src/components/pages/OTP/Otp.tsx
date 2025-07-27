70f362c4fd2808adba5d0d6cba72a7d8f1f38df5
Ade <adesolasolarin@gmail.com>
2025-07-27 14:26:54 +0100

    jf

--- src/components/pages/OTP/Otp.tsx
+++ src/components/pages/OTP/Otp.tsx
@@ -58,7 +58,7 @@ useEffect(() => {
                     type="tel"
                     inputMode="numeric"
                     maxLength={1}
-                    className="w-12 h-12 sm:w-14 sm:h-14 text-lg text-center border border-gray-300 rounded-md shadow-md"
+                    className="p-1 w-11 text-lg text-center border border-gray-300 rounded-md shadow-md"
                     title="otp"
                      pattern="[0-9]*"
                     ref={(el) => { inputRefs.current[idx] = el; }}
