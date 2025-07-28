0d0585aba79344402aa08a90c542d9a467347926
Ade <adesolasolarin@gmail.com>
2025-07-28 19:27:01 +0100

    recent

--- src/components/reuseables/SectionTitle.tsx
+++ src/components/reuseables/SectionTitle.tsx
@@ -1,8 +1,12 @@
-
+import { useLocation } from "react-router-dom";
 
 const SectionTitle = (props:any) => {
+
+  const location = useLocation();
+    const pathname = location.pathname;
+
   return (
-    <div className="min-w-[251px] px-11">
+    <div className={pathname === "/" ? "" : "min-w-[251px] px-11"}>
         <h2 className="text-[24px] font-semibold">{props.h2}</h2>
           <p className="text-[15px]">
             {props.p}
